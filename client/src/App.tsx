a53eba727d84f05c6fbf445f0c293b3cecc1b3e0
akash sah <akashsah946@gmail.com>
2024-04-01 12:15:20 +0530

    style: integrates mui component library

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,8 @@
+import { Button } from "@mui/material"
 function App() {
 
   return (
     <>
-      <p className='text-orange-700'>I am meet app</p>
     </>
   )
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,6 +4,7 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx,html,css}",
   ],
+  important: '#root',
   theme: {
     extend: {},
   },
