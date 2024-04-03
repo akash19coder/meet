4a214fdedb99b3077a89b4a687e8d1efc858e3a9
akash sah <akashsah946@gmail.com>
2024-04-03 16:01:14 +0530

    chore: update src attribute value

--- client/src/Components/MeetLogo.tsx
+++ client/src/Components/MeetLogo.tsx
@@ -3,7 +3,7 @@ import { Box } from '@mui/material'
 const MeetLogo = () => {
   return (
     <Box>
-       <img src="../../public/meet.png" alt="Logo of meet app" className=' w-48'/>  
+       <img src="./meet.png" alt="Logo of meet app" className=' w-48'/>  
     </Box>
   )
 }
